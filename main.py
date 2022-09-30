18fc01fa609fef70faa29e56717e165f0e26ddee
MrDrovE <ilyxa.eletskiya@gmail.com>
2022-09-30 17:49:55 +0400

    2

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+from datetime import date
+
 from flask import Flask, render_template
+
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html',date = date.today(),all_news = 1,persons_today = 2)
 
 
 if __name__ == "__main__":
